14c60a325122a6d6a925a51f9c9bff7659004caf
sherrylee1534 <sherrylee1534@gmail.com>
2022-07-28 11:49:09 +0800

    Minor change

--- Cult-of-Carrot/Assets/Scripts/ESCAPE.cs
+++ Cult-of-Carrot/Assets/Scripts/ESCAPE.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Escape : MonoBehaviour
 {
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("escape"))
